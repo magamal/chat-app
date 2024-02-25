13790176ac5e7bb0c42e394ba5dad75b335db6d3
Mostafa <mostafa.mohammed.sayed.awad@gmail.com>
2024-02-25 16:16:07 +0200

    - added modules un-added files

---
+++ database-service/lib/database_service.dart
@@ -0,0 +1,10 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library;
+
+export 'src/database_service_base.dart';
+export 'src/database_processor.dart' show DataBaseProcessor;
+export 'src/model/db_action_result.dart' show DbActionResult;
+
+// TODO: Export any libraries intended for clients of this package.
---
+++ database-service/test/database_service_test.dart
@@ -0,0 +1,16 @@
+import 'package:database_service/database_service.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
